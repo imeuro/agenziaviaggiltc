33942fba0105e7c6f1229d75e2ee64298cbbd6fc
imeuro <mauro.fioravanzi@gmail.com>
2022-09-12 15:03:37 +0200

    mbah

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1662716387;s:7:"authKey";s:64:"$.4(jKw3327!j?Z#p##.O^Lm-^h5 k4U#OL-0zoQ.D %VVaW,ky]mt*[UVShLw|^";s:7:"version";s:5:"1.0.5";}
+a:4:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"$.4(jKw3327!j?Z#p##.O^Lm-^h5 k4U#OL-0zoQ.D %VVaW,ky]mt*[UVShLw|^";s:7:"version";s:5:"1.0.5";}
