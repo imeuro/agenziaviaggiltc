3b16014632bbf4a23f1984ad45b7b93450963bea
imeuro <mauro.fioravanzi@gmail.com>
2022-09-13 10:29:21 +0200

    mega-update

---
+++ wp-content/plugins/happyforms/core/templates/partials/form-after-submission-links.php
@@ -0,0 +1,3 @@
+<div class="happyforms-form__part happyforms-part happyforms-form-links">
+	<button type="button" class="happyforms-text-button happyforms-fill-out-again"><?php echo $form['fill_out_again_link']; ?></button>
+</div>
