8a12572645b36f10923f9c862cbf187eee2583c1
imeuro <mauro.fioravanzi@gmail.com>
2022-09-14 16:57:00 +0200

    .gitignore upd

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:4:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"$.4(jKw3327!j?Z#p##.O^Lm-^h5 k4U#OL-0zoQ.D %VVaW,ky]mt*[UVShLw|^";s:7:"version";s:5:"1.0.5";}
